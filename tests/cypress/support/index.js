961cff1810c07aeb2cb36e833af3f89d2c223b39
Leandro Viola <lviola@thecoursekey.com>
2022-05-28 16:52:04 -0300

    cypress-universe

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -18,3 +18,8 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// Screenshots for all tests (passing or failing)
+// afterEach(() => {
+//     cy.screenshot()
+// })
